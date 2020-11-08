f6303700fefcb0ae5964aeeed4417c3aafff9a6b
IchHalt <firen8@hotmail.de>
2020-11-08 18:55:02 +0100

    Movement, "Animation" und Project Settings

--- imaginationland/Assets/Script/PlayerMovement.cs
+++ imaginationland/Assets/Script/PlayerMovement.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+ 
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-
+    public Animator animator;
     Vector2 movement;
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +19,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       movement.x= Input.GetAxisRaw("Horizontal");
+       movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
     }
 }
