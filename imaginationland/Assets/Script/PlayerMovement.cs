3f1d1e35272cdf1b4c22a0d44a63c2846d544c03
IchHalt <firen8@hotmail.de>
2020-11-10 21:06:42 +0100

    Idle Allways Idle

--- imaginationland/Assets/Script/PlayerMovement.cs
+++ imaginationland/Assets/Script/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
     Vector2 movement;
+    Vector2 lastMove;
     
     // Start is called before the first frame update
     void Start()
@@ -21,10 +22,25 @@ public class PlayerMovement : MonoBehaviour
     {
        movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
+        if (movement.x > 0 || movement.x < 0)
+        {
+            lastMove.x = movement.x;
+            lastMove.y = movement.y;
+        }
+
+        if (movement.y > 0 || movement.y < 0)
+        {
+            lastMove.x = movement.x;
+            lastMove.y = movement.y;
+        }
+       
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        animator.SetFloat("lastMoveX", lastMove.x);
+        animator.SetFloat("lastMoveY", lastMove.y);
     }
 
     private void FixedUpdate()
