ebb85994f98528360d8134f5eb8d98f8c0acf75c
IchHalt <firen8@hotmail.de>
2020-11-21 23:03:46 +0100

    CameraTest

---
+++ imaginationland/Assets/Script/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform player;
+    private Vector2 camBounds = new Vector2( 5,5);
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(Camera.main.pixelHeight);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        
+
+        if (player.transform.position.x > transform.position.x)
+        {
+            transform.position = new Vector2(camBounds.x * -1, camBounds.y * -1);
+
+        }
+       
+    }
+}
