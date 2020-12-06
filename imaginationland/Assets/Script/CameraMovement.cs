ae06015330aa122507a027353aae3ff50ab82e43
IchHalt <firen8@hotmail.de>
2020-12-06 20:51:17 +0100

    Camera Rumspielerei

--- imaginationland/Assets/Script/CameraMovement.cs
+++ imaginationland/Assets/Script/CameraMovement.cs
@@ -5,26 +5,36 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
 
-    public Transform player;
-    private Vector2 camBounds = new Vector2( 5,5);
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        print(Camera.main.pixelHeight);
+        
 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
      
-        
-
-        if (player.transform.position.x > transform.position.x)
+        if (transform.position != target.position)
         {
-            transform.position = new Vector2(camBounds.x * -1, camBounds.y * -1);
+
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+
+
 
         }
-       
+
+     
     }
 }
